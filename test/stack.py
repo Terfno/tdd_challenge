1d924b60f5a691193cabf60aa1ba8a3692b1a8cb
terfno <sueda.takahito@gmail.com>
2018-09-07 14:05:01 +0900

    交代

--- test/stack.py
+++ test/stack.py
@@ -2,9 +2,12 @@ import unittest
 from stack import STACK
 
 class TestSTACK(unittest.TestCase):
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         stack=STACK()
+
     def test_isEmpty(self):
         self.assertEqual(stack.isEmpty(), True)
+
     def test_push_top(self):
         self.assertEqual(stack.top(),1)
