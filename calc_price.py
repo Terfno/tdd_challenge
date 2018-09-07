da62b55136596c57aa307dcbe947356f482d8183
terfno <sueda.takahito@gmail.com>
2018-09-07 14:51:56 +0900

    テスト成功 lambdaで実装した 多分roundミス

--- calc_price.py
+++ calc_price.py
@@ -1,11 +1,11 @@
-import math
-from decimal import Decimal, ROUND_HALF_UP, ROUND_HALF_EVEN
-
 class Calc_price():
     def calculater_price(self, values):
+        round=lambda x:(x*2+1)//2
         sum = 0
+
         for value in values:
             sum += value
 
-        flo = sum * 1.1
-        return Decimal(str(flo)).quantize(Decimal('1'), rounding=ROUND_HALF_EVEN)
+        ans = sum * 1.1
+        ans = round(ans)
+        return ans
