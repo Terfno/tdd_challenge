028e37a3a26d487eea5fdc4922097c35768e68f0
Satorino01 <satorino0821@gmail.com>
2018-09-07 17:22:36 +0900

    交代

--- calc_price.py
+++ calc_price.py
@@ -1,12 +1,49 @@
+from di_sample import MyMemory
+
 class Calc_price():
+    # def __init__(self, d_input, d_output):
+    #      self.d_input = d_input
+    #      self.d_output = d_output
+
     def calculater_price(self, values):
         round=lambda x:(x*2+1)//2
         sum = 0
-
         for value in values:
-            sum += value
+            sum += int(value)
 
         ans = sum * 1.1
         ans = int(round(ans))
-        print(ans)
         return ans
+
+    def input_to_data(self, input):
+        return [[10,12,3],[40,16],[100,45]]
+        # lines = input.split('\n')
+        # for line in lines:
+        #     value_list = line.split(',')
+
+#    def execute(self):
+
+
+# def some_method():
+#     calc_price = Calc_price()
+#     test_file = open('input.txt')
+#     lines = test_file.readlines()  # 1行毎にファイル終端まで全て読む(改行文字も含まれる)
+#     test_file.close()
+#     for line in lines:
+#         values = line.split(',')
+#         ans = calc_price.calculater_price(values)
+
+
+
+if __name__ == '__main__':
+    # シェル等から実行されたときに実行される内容
+    # ここは他のモジュールから import された時等は実行されない
+    while(True):
+        try:
+            line = input()
+        except EOFError:
+            break
+        values = line.split(',')
+        calc_price = Calc_price()
+        ans = calc_price.calculater_price(values)
+        print(ans)
--- test/calc_price.py
+++ test/calc_price.py
@@ -1,5 +1,9 @@
+import sys
+import io
 import unittest
 from calc_price import Calc_price
+from di_sample import SomeKVSUsingDynamoDB
+
 
 class TestCalculatePrice(unittest.TestCase):
     def test_calculater_price(self):
@@ -12,3 +16,24 @@ class TestCalculatePrice(unittest.TestCase):
         assert 66 == calc_price.calculater_price([20,40])
         assert 198 == calc_price.calculater_price([30,60,90])
         assert 40 == calc_price.calculater_price([11,12,13])
+
+    # def test_execute(self):
+    #     input = io.StringIO('10, 10')
+    #     output = io.StringIO()
+    #     calc_price = Calc_price(input, output)
+    #     calc_price.execute()
+    #     assert output.getvalue() == '22'
+
+    def test_input_to_data(self):
+        calc_price = Calc_price()
+
+        input = '10,12,3\n40,16\n100,45\n'
+        assert [[10,12,3],[40,16],[100,45]] == calc_price.input_to_data(input)
+
+        input = '1,25,3\n40,16\n\n100,45\n'
+        assert [[1,25,3],[40,16],[],[100,45]] == calc_price.input_to_data(input)
+
+
+
+
+
